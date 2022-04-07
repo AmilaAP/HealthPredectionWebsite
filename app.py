978be02ac0662fe9fbb57473145c7ba51463b4ab
Hewarathna Ashen Iranga <48565752+ashen007@users.noreply.github.com>
2022-04-08 00:33:58 +0530

    Update app.py
    i add the main function which is returning your predicted values to result in page. model load to the main in first code block I put comment there make sure to check all paths and routes.

--- app.py
+++ app.py
@@ -2,10 +2,33 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+# change model file location for your model location
+with open(f'model/sk_linear_model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# check route to match your result page location
+@app.route('/result', methods=['GET', 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        return flask.render_template('index.html')
+
+    if flask.request.method == 'POST':
+        # change column values to your symptoms
+        inputs = pd.DataFrame(
+            columns=['LotFrontage', 'MasVnrArea', 'TotalBsmtSF', 'firstFlrSF', 'GrLivArea', 'FullBath',
+                     'TotRmsAbvGrd', 'Fireplaces', 'GarageCars', 'GarageArea', 'MasVnrType', 'OverallQual',
+                     'ExterQual', 'BsmtQual', 'HeatingQC', 'KitchenQual', 'FireplaceQu', 'YearBuilt',
+                     'YearRemodAdd', 'GarageYrBlt', 'Age'])
+        inputs = inputs.append(flask.request.form.to_dict(), ignore_index=True)
+        inputs = inputs.astype(float)
+        result = model.predict(inputs)
+
+        # check return location
+        return flask.render_template('result.html', result=result[0], )
 
 
 @app.route('/predict')
