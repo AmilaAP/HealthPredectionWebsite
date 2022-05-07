4645692dd36d97117914cba34a0773d8d33f021f
ashen iranga hewarathna <asheniranga@gmail.com>
2022-05-07 21:50:03 +0530

    fix model loading errors, create seperate functions to load model. TODO-create input dataframe according to model inputs to get form inputs into model.

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ from flask import render_template
 
 app = Flask(__name__)
 
-# change model file location for your model location
-with open(f'model/sk_linear_model.pkl', 'rb') as file:
-    model = pickle.load(file)
 
 def get_model():
     with open('model/model.pkl', 'rb') as file:
@@ -42,26 +39,6 @@ def make_prediction():
 def index():
     return render_template('index.html')
 
-# check route to match your result page location
-@app.route('/result', methods=['GET', 'POST'])
-def main():
-    if flask.request.method == 'GET':
-        return flask.render_template('index.html')
-
-    if flask.request.method == 'POST':
-        # change column values to your symptoms
-        inputs = pd.DataFrame(
-            columns=['LotFrontage', 'MasVnrArea', 'TotalBsmtSF', 'firstFlrSF', 'GrLivArea', 'FullBath',
-                     'TotRmsAbvGrd', 'Fireplaces', 'GarageCars', 'GarageArea', 'MasVnrType', 'OverallQual',
-                     'ExterQual', 'BsmtQual', 'HeatingQC', 'KitchenQual', 'FireplaceQu', 'YearBuilt',
-                     'YearRemodAdd', 'GarageYrBlt', 'Age'])
-        inputs = inputs.append(flask.request.form.to_dict(), ignore_index=True)
-        inputs = inputs.astype(float)
-        result = model.predict(inputs)
-
-        # check return location
-        return flask.render_template('result.html', result=result[0], )
-
 
 @app.route('/predict')
 def predict():
@@ -72,10 +49,6 @@ def predict():
 @app.route('/result')
 def result():
     return render_template('result.html')
-
-@app.route('/predictnew')
-def predictnew():
-    return render_template('predictnew.html')
 
 
 if __name__ == '__main__':
